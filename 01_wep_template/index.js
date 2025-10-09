1b4612860aa0fcfe151372d592a51ba2bf3860ac
Toke <2ketoplund@gmail.com>
2025-10-09 09:46:17 +0200

    js improvement

--- 01_wep_template/index.js
+++ 01_wep_template/index.js
@@ -3,17 +3,25 @@ var currentPage = '#page5'
 //P5 setup() bliver kaldt EN gang før siden vises 
 function setup(){
     console.log('P5 setup kaldt')
-    //Sæt event listeners op på menu
-    var allMenuItems=selectAll('.sidebar a')
-    allMenuItems.map(
-        function(item){
-            item.mousePressed(
-                function(){
-                    shiftPage(item.attribute('action'))
-                }
+    //Sæt menu op
+    //hent alle sider som array
+    var allPages=selectAll('.page')
+    //run list one by one
+    allPages.map(
+        (page)=>{
+            //made new a tag element
+            var menuItem=createElement('a')
+            //set a tag as html title
+            menuItem.html(page.attribute('title'))
+            // set event lisener
+            menuItem.mousePressed(
+                ()=>shiftPage('#'+page.attribute('id'))
             )
+            //set a tag as sidebar
+            select('.sidebar').child(menuItem)
         }
     )
+
 }
 
 
