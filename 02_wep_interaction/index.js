806c6cf1c72e7e24195f66e3d7fccc89b19572f8
Toke <2ketoplund@gmail.com>
2025-11-03 20:44:11 +0100

    stanley added

--- 02_wep_interaction/index.js
+++ 02_wep_interaction/index.js
@@ -1,16 +1,16 @@
 var currentPage = '#page1'
 
 //P5 setup() bliver kaldt EN gang før siden vises 
-function setup(){
+function setup() {
     console.log('P5 setup kaldt')
 
     //Buttons
-    var theButton =select('#theButton')
+    var theButton = select('#theButton')
     //sæt event listener up on button
-    theButton.mousePressed(()=>{
-        if( confirm('er du sikker?')){
+    theButton.mousePressed(() => {
+        if (confirm('er du sikker?')) {
             theButton.html('i was clicked')
-        }else{
+        } else {
             theButton.html('i dont know who i am')
         }
     })
@@ -19,8 +19,15 @@ function setup(){
     //drop downs
     var theDropdown = select('#theDropdown')
     //event listener changed
-    theDropdown.changed(()=>{
+    theDropdown.changed(() => {
+        select('#page1').style('background-color', theDropdown.value())
         select('#page2').style('background-color', theDropdown.value())
+        select('#page3').style('background-color', theDropdown.value())
+        select('#page4').style('background-color', theDropdown.value())
+        select('#page5').style('background-color', theDropdown.value())
+        select('#page6').style('background-color', theDropdown.value())
+
+
     })
 
 
@@ -28,7 +35,7 @@ function setup(){
     var theInput = select('#theInput')
     var theInputButton = select('#theInputButton')
     var theInputTitle = select('#theInputTitle')
-    theInputButton.mousePressed(()=>{
+    theInputButton.mousePressed(() => {
         var title = theInput.value()
         theInput.hide()
         theInputButton.hide()
@@ -36,8 +43,39 @@ function setup(){
     })
 
 
+    var ck1 = select('#ck1')
+    ck1.mousePressed(() => {
+        if (confirm('er du sikker?')) {
+            shiftPage('#page5')
 
-    
+        } else {
+            alert('stanley was unsure')
+        }
+    })
+
+    var buttonc1 = select('#buttonc1')
+    var buttonc2 = select('#buttonc2')
+    var buttonc3 = select('#buttonc3')
+    //sæt event listener up on button
+    buttonc1.mousePressed(() => {
+        if (confirm('are you sure stanley?')) {
+            shiftPage('#page6')
+        } else {
+            buttonc1.html('stanley was not sure. instead stanley used his time thinking about why donuts have holes.')
+        }
+    })
+    buttonc2.mousePressed(() => {
+        if (confirm('are you sure stanley?')) {
+            shiftPage('#page4')
+        } else {
+            buttonc2.html('stanley was not sure. instead stanley used his time thinking about what might happen if he just did, what the narrator wanted for once. Right stanley?')
+        }
+    })
+    buttonc3.mousePressed(() => {
+        if (confirm('stanley stood still, so still infact that the world moved without stanley. who now felt he was 5 years older.')) {
+
+        }
+    })
 
 
 
@@ -45,17 +83,17 @@ function setup(){
 
     //Sæt menu op
     //hent alle sider som array
-    var allPages=selectAll('.page')
+    var allPages = selectAll('.page')
     //run list one by one
     allPages.map(
-        (page)=>{
+        (page) => {
             //made new a tag element
-            var menuItem=createElement('a')
+            var menuItem = createElement('a')
             //set a tag as html title
             menuItem.html(page.attribute('title'))
             // set event lisener
             menuItem.mousePressed(
-                ()=>shiftPage('#'+page.attribute('id'))
+                () => shiftPage('#' + page.attribute('id'))
             )
             //set a tag as sidebar
             select('.sidebar').child(menuItem)
@@ -65,7 +103,17 @@ function setup(){
 }
 
 
-function shiftPage(newPage){
+
+
+function shiftPage(newPage) {
+    if (newPage == '#page5') {
+        var buttonc1 = select('#buttonc1')
+        var buttonc2 = select('#buttonc2')
+        var buttonc3 = select('#buttonc3')
+        buttonc1.html('walk in');
+        buttonc2.html('close door');
+        buttonc3.html('stand still');
+    }
     select(currentPage).removeClass('show')
     select(newPage).addClass('show')
     currentPage = newPage
