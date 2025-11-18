b5ebb9be8dd89332c30d73d65428e58dc55c8094
Toke <2ketoplund@gmail.com>
2025-11-18 11:02:55 +0100

    movie website

---
+++ 00_HTTYD/index.js
@@ -0,0 +1,43 @@
+var currentPage = '#page5'
+var videoButton, theVideo
+var videoPlaying = true
+
+//P5 setup() bliver kaldt EN gang før siden vises 
+function setup(){
+    console.log('P5 setup kaldt')
+
+    //shift to current page
+    shiftPage(currentPage)
+
+    //the video
+    theVideo = select('#theVideo')
+    //video control
+    videoButton = select('#videoButton')
+        
+    //Sæt menu op
+    //hent alle sider som array
+    var allPages=selectAll('.page')
+    //run list one by one
+    allPages.map(
+        (page)=>{
+            //made new a tag element
+            var menuItem=createElement('a')
+            //set a tag as html title
+            menuItem.html(page.attribute('title'))
+            // set event lisener
+            menuItem.mousePressed(
+                ()=>shiftPage('#'+page.attribute('id'))
+            )
+            //set a tag as sidebar
+            select('.sidebar').child(menuItem)
+        }
+    )
+
+}
+
+
+function shiftPage(newPage){
+    select(currentPage).removeClass('show')
+    select(newPage).addClass('show')
+    currentPage = newPage
+}
