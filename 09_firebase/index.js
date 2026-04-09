3029cff03ab4e3b6845f933e25c5cfe9541d6fa9
Toke <2ketoplund@gmail.com>
2026-04-09 09:45:52 +0200

    added quotes to implement in firebase later

--- 09_firebase/index.js
+++ 09_firebase/index.js
@@ -2,13 +2,13 @@
 
 
 //make ref to collection
-var testRef = db.collection('test')
-console.log('set up ref to test')
+var quotesRef = db.collection('quotes_data')
+console.log('set up ref see quote data')
 
 //P5 setup() bliver kaldt EN gang før siden vises 
 function setup(){
     //THE GREAT REASON: ONSNAPSHOT
-    testRef.onSnapshot( snap => {
+    quotesRef.onSnapshot( snap => {
         console.log('recived snap', snap.size)
         snap.forEach( doc => {
             var d = doc.data()
@@ -17,3 +17,22 @@ function setup(){
     })
 }
 
+function keyPressed(){
+    //console. log(key)
+    if(key == "Enter"){
+        //hent teksten fra input feltet
+        var q = select('#newQuote').value()
+        if(q == ""){
+            confirm('skriv venligst noget før du trykker enter')
+            return
+        }
+        //nu skal vi gemme det nye quote i firestore
+        //funktionen add() på en collectionref
+        //opretter en ny collection hvis den ikke findes
+        quotesRef.add({
+            text: q,
+            timestamp: firebase.firestore.fieldValue.serverTimestamp()
+            //.then kaldes asynkront når add er færdig
+        })
+    }
+}
