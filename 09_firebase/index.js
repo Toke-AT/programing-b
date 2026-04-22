b97495b33035dc271a8e816be473dda701eb1886
Toke <2ketoplund@gmail.com>
2026-04-22 12:56:22 +0200

    added stuff with onsnapshot

--- 09_firebase/index.js
+++ 09_firebase/index.js
@@ -7,32 +7,58 @@ console.log('set up ref see quote data')
 
 //P5 setup() bliver kaldt EN gang før siden vises 
 function setup(){
-    //THE GREAT REASON: ONSNAPSHOT
+    //NU KOMMER DET GENIALE: ONSNAPSHOT 
     quotesRef.onSnapshot( snap => {
-        console.log('recived snap', snap.size)
+        select('#quotes').html('')
+        console.log('Modtog snap', snap.size)
+        //ryd quotes div og sæt de nye quotes ind
         snap.forEach( doc => {
             var d = doc.data()
             console.log(d)
-        });
+            // Opret quote kort med knapper
+            select('#quotes').child(
+                createDiv().addClass('card').child(
+                    createDiv(d.text)
+                ).child(
+                    createDiv(d.timestamp.toDate().toLocaleDateString("da-DK",{
+                        month: "short",
+                        weekday:"long",
+                        year: "numeric"
+                    })).addClass('date')
+                ).child(
+                    createImg('./assets/delete.svg')
+                    .addClass('delete')
+                    .mousePressed( ()=>{
+                        if(confirm("ER du nu sikker på at du vil slette - permanent og uopretteligt - dit quote: " + d.text)){
+                            quotesRef.doc(doc.id).delete()
+                        }
+                    } )
+                )
+            )
+        })
     })
 }
 
+//key pressed er en indbygget p5.js funktion 
 function keyPressed(){
-    //console. log(key)
+    //console.log(key)
     if(key == "Enter"){
         //hent teksten fra input feltet
         var q = select('#newQuote').value()
-        if(q == ""){
-            confirm('skriv venligst noget før du trykker enter')
+        if(q == "") {
+            confirm('skriv venligst noget FØR DU TRYKKER ENTER')
             return
         }
         //nu skal vi gemme det nye quote i firestore
-        //funktionen add() på en collectionref
-        //opretter en ny collection hvis den ikke findes
+        //funktionen add() på en collectionref 
+        //OPRETTER en ny collection hvis den IKKE findes 
         quotesRef.add({
             text: q,
-            timestamp: firebase.firestore.fieldValue.serverTimestamp()
-            //.then kaldes asynkront når add er færdig
-        })
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            //.then kaldes asynkront NÅR add er færdig
+        }).then(
+            console.log('Quote gemt i databasen', q)
+        )
+        select('#newQuote').html('')
     }
 }
