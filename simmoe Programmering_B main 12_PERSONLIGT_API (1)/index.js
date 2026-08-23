d822bed12648ee42d9ae3cf5dcf787f1931c5b92
Toke <2ketoplund@gmail.com>
2026-08-23 12:53:14 +0200

    added silas api code

--- simmoe Programmering_B main 12_PERSONLIGT_API (1)/index.js
+++ simmoe Programmering_B main 12_PERSONLIGT_API (1)/index.js
@@ -1,20 +1,28 @@
 
-
-//P5 setup() bliver kaldt EN gang før siden vises 
+var currentPage = "#page1"
+var elev =["Selma","Silas","Toke","Viggo","Balder"]
+//P5 setup() bliver kaldt EN gang fÃ¸r siden vises 
 function setup(){
     noCanvas()
     // Brug funktionerne fra dit personlige API her.
-    showToast('SELMA ER FUCKING NICE', 500, type="notify")
-    console.log('selma')
+    showToast('Opgaven skal afleveres senest kl. 22', 5000, "notify")
+         select("#btn1").mousePressed(()=>{
+            createList(elev, "elevbox", "elevcss")
+        })
+        select("#btn2").mousePressed(()=>{
+        var randomElev = randomFrom(elev)
+        select("#randomElevBox").html(randomElev)
+        shiftPage("#page2")
+        })
+        select("#backBtn2").mousePressed(()=>{
+            shiftPage("#page1")
+        })
 
-    startTimer()
-    setTimeout(()=>{
-            stopTimer()
-            
-        }, 4000)
-    
-
-
-
-    
+       
+        select("#timerBtn").mousePressed(()=>{  
+        shiftPage("#page3")
+        var seconds = Number(select("#timerInput").value()) 
+        startTimer(seconds, "timer")  
+        })
+        
 }
