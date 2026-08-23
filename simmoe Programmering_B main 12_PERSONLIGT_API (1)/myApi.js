557c3e4da6862cf7e9119bc2f7ace6392d636369
Toke <2ketoplund@gmail.com>
2026-08-23 14:35:11 +0200

    re added mqtt

--- simmoe Programmering_B main 12_PERSONLIGT_API (1)/myApi.js
+++ simmoe Programmering_B main 12_PERSONLIGT_API (1)/myApi.js
@@ -61,7 +61,45 @@ function stopTimer() {
     console.log('timer done')
 }
 
+//mqtt er et objekt vi får fra mqtt bilbioteket i html siden 
+var client
 
+    client = mqtt.connect('wss://mqtt.nextservices.dk')
+
+    client.on('connect', msg => {
+        console.log(msg)
+        console.log('Forbundet til NEXT MQTT server')
+        toast.html('Forbundet til NEXT MQTT server')
+        toast.addClass('online')
+        toast.addClass('toastShow')
+        setTimeout(()=>{
+            toast.removeClass('online')
+            toast.removeClass('toastShow')
+        }, 2000)
+    })
+
+    client.subscribe('toke')
+    client.subscribe('toke/page')
+
+    //Her får vi beskeder på forskellige topics vi abonnerer på 
+    client.on('message', (topic, msg) => {
+        console.log(topic, msg.toString())
+        msg = msg.toString()
+        if(topic == 'toke/page'){
+            console.log('nu skal der skiftes side')
+            //ER DET ET TAL?
+            msg = '#page' + msg
+            shiftPage(msg)
+        }
+        //NU SKAL DER SKE NOGET SPÆNDENDE
+        if(topic == 'toke'){
+            select('#msg').elt.textContent = 'Besked på topic ' + topic + ' med teksten ' + msg
+        }
+    })
+
+    client.publish('programmering/page', '1')
+
+    var currentPage = "#page1"
 
 
 
